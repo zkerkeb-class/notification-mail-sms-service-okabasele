be0212d216657789d70187f491559babc42a9efd
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2025-07-06 04:35:31 +0200

    feat: add CORS support to the notification service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const env = require("./config/env")
 const apiRouter = require("./routes")
 const errorHandling = require("./middlewares/errorHandling")
 const logger = require("./middlewares/logger")
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(logger)
 app.use("/api", apiRouter)
